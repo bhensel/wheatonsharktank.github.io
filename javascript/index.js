d9df7d1957fbde5c8aa96ab62c57b6b8e263088f
Chris Anderson <hi@chrisanderson.io>
2014-10-15 23:44:24 -0500

    Updated to include registration

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
 
 // set the date we're counting down to
-var target_date = moment("2014-10-15T21:00:00").toDate().getTime();
+var target_date = moment("2014-11-13T21:00:00").toDate().getTime();
 
 // variables for time units
 var days, hours, minutes, seconds; 
@@ -30,3 +30,35 @@ setInterval(function() {
 	+ minutes + " minutes, " + seconds + " seconds";   
 
 }, 1000);
+
+
+$(document).ready(function() {
+    
+    $(".registration").submit(function(e) {
+        
+        var values = [];
+        values.push($("textarea[name='shark_tank_idea']"));
+        values.push($("textarea[name='describe_customer']"));
+        values.push($("textarea[name='team_skills']"));
+        values.push($("textarea[name='kingdom_god']"));
+        values.push($("textarea[name='won_what_now']"));
+        values.push($("textarea[name='idea_excel']"));
+        
+        var shouldReturn = true;
+        
+        for (var i = 0; i < values.length; i++) {
+            if (values[i].val().length <= 0) {
+                values[i].addClass("red");
+                shouldReturn = false;
+            } else {
+                values[i].removeClass("red");
+            }
+        }
+        
+        if (!shouldReturn) {
+            alert("Please fill in all of the fields!");
+        }
+        
+        return shouldReturn;
+    });
+});
